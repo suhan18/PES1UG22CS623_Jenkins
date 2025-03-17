29d8da4388482431c9bf7db57060d2c91155f53b
suhan18 <134138944+suhan18@users.noreply.github.com>
2025-03-17 12:10:13 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout << "I have made changes\n";
+  cout << "I have made changes to hello.cpp\n";
   return 0;
 }
